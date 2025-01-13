c01bb30fb72d759f7ba71f21e8a938a91c2b9669
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-13 09:41:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# store-fake-store-api [React & Tailwind]
+ Creating a fake store with the fake store API
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return <div className='overflow-hidden'>react app</div>;
+};
+
+export default App;
