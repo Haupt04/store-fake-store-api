f2bb430a2be83aff0df085e7ca30330ecfc5493e
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-13 16:14:45 +0200

    Fixed Cart Issue

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import {IoIosAdd, IoMdClose, IoMdRemove} from 'react-icons/io'
+import { CartContext } from '../contexts/CartContext';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({item}) => {
+  const { removeFromCart } = useContext(CartContext)
+  const {id, title, image, price, amount} = item
+  return (
+    <div className='flex justify-between items-center gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
+  {/* Left Side: Image */}
+  <Link to={`/product/${id}`}>
+    <img className="max-w-[80px]" src={image} alt={title} />
+  </Link>
+
+  {/* Right Side: Text Content */}
+  <div className='flex flex-col w-full'>
+    {/* Title and Remove Icon */}
+    <div className='flex justify-between mb-2'>
+      <Link to={`/product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+      <div className='text-xl cursor-pointer' onClick={() => removeFromCart(id)}>
+        <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
+      </div>
+    </div>
+
+    {/* Quantity Selector, Single Item Price & Total Price INLINE */}
+    <div className='flex items-center justify-between h-[36px] text-sm'>
+      {/* Quantity Selector */}
+      <div className='flex max-w-[100px] items-center h-full border text-primary font-medium'>
+        <div className='flex-1 flex justify-center items-center cursor-pointer'>
+          <IoMdRemove />
+        </div>
+        <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+        <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+          <IoIosAdd />
+        </div>
+      </div>
+
+      {/* Single Item Price */}
+      <div className='text-gray-500 font-medium'>{`$ ${price.toFixed(2)}`}</div>
+
+      {/* Total Price (IN LINE) */}
+      <div className='text-primary font-medium'>{`$ ${(price * amount).toFixed(2)}`}</div>
+    </div>
+  </div>
+</div>
+
+
+  
+
+  );
 };
 
 export default CartItem;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,15 +8,31 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const {isOpen, handleClose} = useContext(SidebarContext);
-  const {} = useContext(CartContext);
+  const { cart} = useContext(CartContext);
+
   return (
-  <div className={`${isOpen ? 'right-0' : 'right-full'} w-full bg-white fixed top-0 h-full shadow-2xl m:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
-    <div className='flex items-center justifty-between py-6 border-b'>
+  <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+    <div className='flex items-center justify-between py-6 border-b'>
     <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
     {/* icons  */}
     <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
       <IoMdArrowForward className='text-2xl'/>
     </div>
+    </div>
+    <div>{cart.map((item) => {
+      return <CartItem item={item} key={item.id} />
+    })}
+    </div>
+
+    <div className='bg-pink-200'>
+      {/* total  */}
+      <div>
+        <span>Total: </span>
+      </div>
+      {/* clear cart icon  */}
+      <div className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 justify-center items-center text-xl'>
+        <FiTrash2 />
+      </div>
     </div>
   </div>
   
