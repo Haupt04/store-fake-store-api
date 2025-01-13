7c16aac90f07a90fb8a6ce9a1f2d4dec872c4088
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-13 11:42:30 +0200

    Created View, AddtoCart Btn on Product

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Product from '../components/Product'
 import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
-  const { products} = useContext(ProductContext);
+  const { products } = useContext(ProductContext);
   const filteredProducts = products.filter((item) => {
     return item.category === "men's clothing" || item.category === "women's clothing"
   })
@@ -13,7 +13,7 @@ const Home = () => {
       <div className='container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
           {filteredProducts.map(product => {
-            return <Product />
+            return <Product key={product.id} product={product}/>
           })}
         </div>
       </div>
