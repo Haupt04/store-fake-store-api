6574545dc68bd66319ba3c64f769b7de519439f6
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-13 11:45:16 +0200

    Completed Product Component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React,{ useContext} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import {BsPlus, BsEyeFill } from 'react-icons/bs'
 
@@ -28,13 +28,13 @@ const Product = ({ product}) => {
     </div>
     {/* {category & title & price } */}
     <div>
-      <div>{category}</div>
+      <div className='text-sm capitalize text-gray-500 mb-1'>{category}</div>
       <Link to={`/product/${id}`}>
         <h2 className='font-semibold mb-1'>{title}</h2>
       </Link>
-      <div>{price}</div>
+      <div className='font-semibold'>$ {price}</div>
     </div>
-    <div>2</div>
+    
   </div>;
 };
 
